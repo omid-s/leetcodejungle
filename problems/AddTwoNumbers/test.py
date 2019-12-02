5ecf1ffd7766458a0eeca1e5b942532e68825a3b
Omid <os14526@uga.edu>
2019-12-01 22:40:40 -0500

    adds test for the add two numbers

---
+++ problems/AddTwoNumbers/test.py
@@ -0,0 +1,28 @@
+from problems.AddTwoNumbers.solution_1 import Solution
+from classes.Tools import linked_list_to_list
+from classes.Tools import print_linked_list
+from classes.Tools import list_to_linked_list
+
+inputs = [
+    [[2, 7, 1, 1], [2, 2, 1]],
+    [[4, 5, 3, 6], [4, 5, 3, 6]]
+]
+
+outputs = [
+    [4, 9, 2, 1],
+    [8, 0, 7, 2, 1]
+]
+
+for index in range(len(inputs)):
+    l1 = list_to_linked_list(inputs[index][0])
+    l2 = list_to_linked_list(inputs[index][2])
+
+    print_linked_list(l1)
+    print_linked_list(l2)
+    print("----------------------")
+    ret = linked_list_to_list(Solution().addTwoNumbers(l1, l2))
+    print(outputs[index], "-->", ret)
+
+    assert ret == outputs[index]
+
+print("All tests passed")
