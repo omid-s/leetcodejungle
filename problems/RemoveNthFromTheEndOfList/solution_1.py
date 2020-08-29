967c869983407c54052f5badd89e3db9519b171c
Omid <os14526@uga.edu>
2020-08-29 19:27:46 -0400

    adds solution and analysis to remove nth to the last

--- problems/RemoveNthFromTheEndOfList/solution_1.py
+++ problems/RemoveNthFromTheEndOfList/solution_1.py
@@ -1,5 +1,40 @@
-from  classes.ListNode import ListNode
+from classes.ListNode import ListNode
+
+"""
+# Info 
+removes  the nth to the end of a list in one pass
+
+# Results
+
+Runtime: 24 ms, faster than 98.54% of Python3 online submissions for Remove Nth Node From End of List.
+Memory Usage: 13.8 MB, less than 67.09% of Python3 online submissions for Remove Nth Node From End of List.
+
+
+# Anlaysis 
+The linked list is travered exactly once to the end, thus the runtime is of O(n) where n is the 
+length of the list. 
+Space complexity is of O(n+1) as there is one new item created to handle pre-head node which makes
+the solution of O(n) space as well. 
+
+"""
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        return None
+        temp_head = head
+        n_1 = ListNode("")
+        n_1.next = head
+        cntr = 0
+
+        while temp_head.next:
+            cntr += 1
+            if cntr >= n:
+                n_1 = n_1.next
+
+            temp_head = temp_head.next
+
+        if n_1.next == head:
+            head = head.next
+        else:
+            n_1.next = n_1.next.next
+
+        return head
--- problems/RemoveNthFromTheEndOfList/test.py
+++ problems/RemoveNthFromTheEndOfList/test.py
@@ -4,16 +4,21 @@ from classes.Tools import *
 
 inputs = [
     ["1->2->3->4->5", 2],
-    ["1->2->3", 3]
+    ["1->2->3", 3],
+    ["1->2->3", 1],
+    ["1->2", 1]
+
 ]
 outputs = [
     "1->2->3->5",
-    "2->3"
+    "2->3",
+    "1->2",
+    "1"
 ]
 
 for index in range(len(inputs)):
     ret = Solution().removeNthFromEnd(str2LinkedList(inputs[index][0]), inputs[index][1])
     print(outputs[index], " --> ", linkedlist2Str(ret))
-    assert linkedlist2Str(ret) == linkedlist2Str(outputs[index])
+    assert linkedlist2Str(ret) == outputs[index]
 
 print("All tests passed")
