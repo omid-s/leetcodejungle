767aeed3bb7605a0e9d004003c15c2f20b2cc12b
Omid <os14526@uga.edu>
2020-10-07 17:40:22 -0400

    adds mergeKlists

---
+++ problems/MergeKLists/solution_1.py
@@ -0,0 +1,6 @@
+from classes.ListNode import ListNode
+
+
+class Solution:
+    def mergeKLists(self, lists) -> ListNode:
+        return None
---
+++ problems/MergeKLists/test.py
@@ -0,0 +1,25 @@
+from problems.MergeKLists.solution_1 import Solution
+from classes.Tools import str2LinkedList
+from classes.Tools import *
+
+inputs = [
+    ["1->4->5","1->3->4","2->6"],
+    ["1", "2->3->4->5"],
+    ["1->2", "1"],
+    ["1", ""]
+
+]
+outputs = [
+    "1->1->2->3->4->4->5->6",
+    "1->2->3->4->5",
+    "1->2->3->4->5",
+    "1->1->2",
+    "1"
+]
+
+for index in range(len(inputs)):
+    ret = Solution().mergeKLists(inputs[index])
+    print(outputs[index], " --> ", linkedlist2Str(ret))
+    assert linkedlist2Str(ret) == outputs[index]
+
+print("All tests passed")
