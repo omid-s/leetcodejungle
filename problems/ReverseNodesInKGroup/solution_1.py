3bbe9f3fa0d24815bb75d5ec48c700a4c44e534f
Omid <os14526@uga.edu>
2020-10-10 13:47:10 -0400

    adds reverse k group nodes

---
+++ problems/ReverseNodesInKGroup/solution_1.py
@@ -0,0 +1,10 @@
+from classes.ListNode import ListNode
+"""
+
+
+
+"""
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        return None
---
+++ problems/ReverseNodesInKGroup/test.py
@@ -0,0 +1,25 @@
+from problems.ReverseNodesInKGroup.solution_1 import Solution
+from classes.Tools import str2LinkedList
+from classes.Tools import *
+
+inputs = [
+    ["1->3->5", 2],
+    ["1->2->3->4->5", 3],
+    ["2->3->4->5", 1],
+    ["1->2", 2],
+    ["1", 1]
+]
+outputs = [
+    "3->1->5",
+    "3->2->1->4->5",
+    "3->2->5->4",
+    "2->1",
+    "1"
+]
+
+for index in range(len(inputs)):
+    ret = Solution().reverseKGroup(str2LinkedList(inputs[index][0]),inputs[index][1])
+    print(outputs[index], " --> ", linkedlist2Str(ret))
+    assert linkedlist2Str(ret) == outputs[index]
+
+print("All tests passed")
